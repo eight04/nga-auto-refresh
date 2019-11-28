849df4331e139354aed77968e9750bf4f859c494
eight <eight04@gmail.com>
2019-11-29 04:31:45 +0800

    Release v0.1.0

--- nga-auto-refresh.user.js
+++ nga-auto-refresh.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name NGA Auto Refresh
-// @version 0.0.0
+// @version 0.1.0
 // @description Refresh NGA post automatically.
 // @license MIT
 // @author eight04 <eight04@gmail.com> (https://github.com/eight04)
 // @homepageURL https://github.com/eight04/nga-auto-refresh
 // @supportURL https://github.com/eight04/nga-auto-refresh/issues
-// @include *://bbs.nga.cn/read.php*
+// @match *://bbs.nga.cn/read.php*
 // @grant none
 // ==/UserScript==
 
